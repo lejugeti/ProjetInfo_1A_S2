d9fd9b51e1a817c709141cc46ff05deb4d751cff
Antoine PARIZE <lejugeti@gmail.com>
2020-05-13 17:33:01 +0200

    petits fix pas liés aux recherches

--- Program/Program.cs
+++ Program/Program.cs
@@ -9,31 +9,57 @@ using System.Xml.Serialization;
 namespace Program
 {
     class Program
-    {
+    {   /*
+        * Permet d'afficher un ensemble de choix associés à un nombre pour que l'utilisateur indique sa réponse
+        * @arg enonce, l'énoncé du choix que doit réaliser l'utilisateur 
+        * @arg choix, le tableau des différents choix proposés à l'utilisateur
+        */
+        public static void AfficherGrilleChoix(string enonce, string[] choix)
+        {
+            Escapes space = new Escapes(enonce);
+            Console.Write(enonce);
+
+            foreach(string c in choix)
+            {
+                Console.Write($" |{c}");
+
+            }
+            Console.WriteLine("");
+
+            space.Print(); //on écrit l'espace correspondant à l'énoncé du choix pour décaler ce qu'on va écrire ensuite
+
+            int i = 1;
+            foreach (string c in choix)
+            {
+                Console.Write($"{i} |");
+            }
+            Console.WriteLine("");
+        }
         static void Main(string[] args)
         {
             Role role = new Role("tuteur");
             Matiere matiere = new Matiere("cognition", "666");
             Projet.Date date = new Projet.Date("20/10/2020");
             Intervenant encadrant = new Enseignant("lespinet", "véro", new Role[] { role, role }, "ISM", matiere);
-            Livrable livrable = new Livrable("vidéo", date, "ceci est un livrable", encadrant);
+            Livrable livrable = new Livrable("vidéo", date, "ceci est un livrable");
             Enseignant enseignant = new Enseignant("lespinet", "véro", new Role[] { role, role }, "ISM", matiere);
             Intervenant[] plusieursIntervenants = new Intervenant[] { encadrant, encadrant };
             Eleve eleve = new Eleve("Parize", "antoine", new Role[] { role }, "2022", "1A");
-            Projet projet = new Projet("projet de gestion de projets", "info", 1, new string[] { "2022", "2020" }, "bimbamboom", new string[] { "projet" },
-                new Livrable[] { livrable , livrable}, plusieursIntervenants, new Intervenant[] { encadrant }, new Eleve[] { eleve }, date, date);
+            Projet projet = new Projet("projet de gestion de projets", "info", 1, new string[] { "2022", "2020" }, "bimbamboom", new string[] { "projet" },  encadrant,
+                 new Livrable[] { livrable, livrable }, plusieursIntervenants, new Intervenant[] { encadrant }, new Eleve[] { eleve }, date, date);
 
             Intervenant test = new Eleve("Parize", "antoine", new Role[] { role }, "2022", "1A");
             Externe externe = new Externe("Parize", "antoine", new Role[] { role }, "2022");
 
             //projet.PrintInfos();
-            Catalogue a = new Catalogue();
-            a.AddProjet(projet);
-            a.PrintCatalogue();
+            //Catalogue a = new Catalogue();
+            /*a.AddProjet(Projet.CreateProjet());
+            a.Projets[1].PrintInfos();*/
+
+            //a.PrintCatalogue();
 
 
-
-
+            AfficherGrilleChoix("bonjour", new string[] { "un", "deux", "trois"});
 
             Console.ReadKey();
         }
